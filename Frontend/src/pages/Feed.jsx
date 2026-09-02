46774d4c27e3d739d6c09ef15ce19490085080c7
Ravi Hiremath <hiremathravi333@gmail.com>
2026-09-03 00:45:46 +0530

    Update posts API integration

--- Frontend/src/pages/Feed.jsx
+++ Frontend/src/pages/Feed.jsx
@@ -3,13 +3,15 @@ import axios from "axios"
 
 const Feed = () => {
 
-  const [posts, setPosts] = useState([
-    {
-      _id:"1",
-      image:"https://images.unsplash.com/photo-1706980709179-2ad906cd6f91?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGJhYnklMjBpbWFnZXN8ZW58MHwxfDB8fHww",
-      caption:"Beautiful scenery"
-    }
-  ])
+  // const [posts, setPosts] = useState([
+  //   {
+  //     _id:"1",
+  //     image:"https://images.unsplash.com/photo-1706980709179-2ad906cd6f91?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGJhYnklMjBpbWFnZXN8ZW58MHwxfDB8fHww",
+  //     caption:"Beautiful scenery"
+  //   }
+  // ])
+
+  const [posts, setPosts] = useState([])
 
 useEffect(() => {
   // axios.get("http://localhost:3000/posts")
