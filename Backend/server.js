ea9ef25de5fd4666a74165b8f79c40fcdcf318e7
Ravi Hiremath <hiremathravi333@gmail.com>
2026-09-03 00:07:34 +0530

    prepare backend for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -5,12 +5,14 @@ const connectDB = require('./src/db/db')
 
 // calling the fnc
 connectDB()
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("server is running on port 3000");
-    });
-  })
-  .catch((err) => {
-    console.error("Failed to connect to DB:", err);
-    process.exit(1);
-  });
+  // .then(() => {
+  //   app.listen(3000, () => {
+  //     console.log("server is running on port 3000");
+  //   });
+  // })
+  // .catch((err) => {
+  //   console.error("Failed to connect to DB:", err);
+  //   process.exit(1);
+  // });
+
+  module.exports = app ;
